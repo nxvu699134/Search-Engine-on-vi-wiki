c245ccfd5069386703a61f701ed68bdc3d99571c
Nguyen Xuan Vu <nxvu699134@gmail.com>
2017-04-21 22:22:21 +0700

    v1.1

--- Source/Main.py
+++ Source/Main.py
@@ -8,17 +8,19 @@ DATA_NAME = 'data.txt'
 def main():
     Setup_logging();
     arg_parser = argparse.ArgumentParser();
-    arg_parser.add_argument("[DATABASE_NAME]", type=str, help="Name of database file <.txt> or <.zip>");
-    arg_parser.add_argument("-q", "--query", nargs=1, help="Search with a query");
-    arg_parser.add_argument("-f", "--file", nargs=1, help="Search with a file that contains some queries");
+    arg_parser.add_argument("-b", "--build", nargs=1, type=str, metavar='DATABASE_FILE',
+                            help="Build model with database file. Name of database file <.txt> or <.zip>");
+    arg_parser.add_argument("-q", "--query", nargs=1, type=str, help="Search with a query");
+    arg_parser.add_argument("-qf", "--queryfile", nargs=1, type=str,
+                            help="Search with a file that contains some queries");
     args = arg_parser.parse_args()
-    p = Process(DATA_NAME);
+    p = Process();
     if args.query:
-        p.Run(True, query=args.query[0]);
+        p.Search(True, query=args.query[0]);
     elif args.file:
-        p.Run(False, filename=args.file[0]);
-    # else:
-    #     print args.help
+        p.Search(False, filename=args.queryfile[0]);
+    elif args.build:
+        p.Build(args.build[0]);
 
 
 if '__main__' == __name__:
--- Source/Process.py
+++ Source/Process.py
@@ -9,12 +9,9 @@ class Process:
     NUM_PAGES_PER_THREAD = 5000;
     INDEX_FOLDER = 'indexdir';
     INDEX_NAME = 'wiki';
-    CACHE_FOLDER = 'Cache';
 
-    def __init__(self, database_filename):
+    def __init__(self):
         self.entries = [];
-        self.database_filename = database_filename;
-        self.Cache = set();
 
     def __Readfile(self, filename):
         rd = Reader(filename);
@@ -27,69 +24,29 @@ class Process:
         indexer = Indexer(folder=Process.INDEX_FOLDER, name=Process.INDEX_NAME, entries=self.entries);
         indexer.Index();
 
-    def __Training(self):
-        if self.__Readfile(self.database_filename):
-            self.__Indexing();
-
-    '''mode : True : query with string, file is otherwise'''
-    def Run(self, mode, filename=None, query=None):
+    def Build(self, database_filename):
         if not os.path.exists(Process.INDEX_FOLDER):
             os.mkdir(Process.INDEX_FOLDER);
 
         if not Indexer.Check_exists(Process.INDEX_FOLDER, Process.INDEX_NAME):
             print "Building model .............";
             start_time = time.time();
-            self.__Training();
+            if self.__Readfile(database_filename):
+                self.__Indexing();
             print "Done, Building time: %f" % (time.time() - start_time);
+
+    '''mode : True : query with string, file is otherwise'''
+    def Search(self, mode, filename=None, query=None):
+        if not os.path.exists(Process.INDEX_FOLDER) \
+                or not Indexer.Check_exists(Process.INDEX_FOLDER, Process.INDEX_NAME):
+            print "Please run : python Main.py --build <DATABASE_FILE>"
+            return;
 
         ix = Indexer(folder=Process.INDEX_FOLDER, name=Process.INDEX_NAME);
         if mode:
-            print query
             query = query.decode('utf-8');
             results = ix.QueryString(query);
             Utils.PrintResult(results);
         else:
             ix.QueryFile(filename);
             print "Please check result in %s folder !" % Indexer.RESULT_QUERY_FOLDER;
-
-
-
-    # def __LoadCache(self):
-    #     try:
-    #         os.mkdir(Process.CACHE_FOLDER);
-    #     except:
-    #         for file in os.listdir(Process.CACHE_FOLDER):
-    #             if file.endswith(".txt"):
-    #                 file = file.decode(encoding='utf-8');
-    #                 self.Cache.add(Utils.FilenameToURL(file));
-    #
-
-    #
-    # def __CreateCrawlThread(self):
-    #     threads = [];
-    #     num_thread = len(self.entries) / Process.NUM_PAGES_PER_THREAD;
-    #     print num_thread
-    #     for i in range(0, num_thread):
-    #         begin = Process.NUM_PAGES_PER_THREAD * i;
-    #         end = begin + Process.NUM_PAGES_PER_THREAD;
-    #         thread = CrawlThread(i, "Thread-" + str(i), [self.entries[j] for j in range(begin, end)]);
-    #         thread.start();
-    #         threads.append(thread);
-    #
-    #     if 0 < len(self.entries) % Process.NUM_PAGES_PER_THREAD:
-    #         begin = Process.NUM_PAGES_PER_THREAD * num_thread;
-    #         end = begin + len(self.entries) % Process.NUM_PAGES_PER_THREAD;
-    #         thread = CrawlThread(num_thread, "Thread-"+str(num_thread),
-    #                              [self.entries[j] for j in range(begin, end)])
-    #         thread.start();
-    #         threads.append(thread);
-    #
-    #     for thread in threads:
-    #         thread.join();
-    #
-    #     return threads;
-    #
-    #
-
-
-
